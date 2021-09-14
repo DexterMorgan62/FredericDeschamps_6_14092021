ade14560bd168ca43dd0414906fe618022956727
Deschamps <deschfred62@yahoo.fr>
2021-09-14 22:38:43 +0200

    feat: express

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur !');
-});
+// importer application app.js
+const app = require("./app");
+
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
