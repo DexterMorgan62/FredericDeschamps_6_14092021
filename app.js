5098f63544e94a86f56002714e9ed873b5ee8699
Deschamps <deschfred62@yahoo.fr>
2021-09-20 20:40:22 +0200

    feat: dependances dotenv morgan

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const express = require('express');
+const express = require("express");
+
+const morgan = require("morgan");
 
 const app = express();
 
+// log requetes responses
+app.use(morgan("dev"));
+
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    res.status(201);
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
-  });
+  console.log("Requête reçue !");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  res.json({ message: "Votre requête a bien été reçue !" });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Réponse envoyée avec succès !");
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const http = require("http");
 // importer application app.js
 const app = require("./app");
 
+const dotenv = require("dotenv");
+const result = dotenv.config();
+app.set("port", process.env.PORT);
+
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT);
